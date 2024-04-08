c8e0903bb315b55f773cfc2bd015cf5db6cc1c9e
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-04-08 22:32:15 +0545

    Add leave controller and route for leave from requests

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+const leaveController = require('./../controllers/leavecontroller');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/leaveformrequest', leaveController.leaveSchema);
 
-module.exports = router;
+module.exports = router; 
