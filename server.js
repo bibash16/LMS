c374e4902c3b4b11b960d2a9eae81c95b3c863ad
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-04-10 10:26:04 +0545

    login&signup

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express =require('express');
+// Set up Express server
+const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const path = require('path'); 
+const path = require('path');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+const app = express();
 
+// MongoDB connection setup
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
@@ -14,15 +16,26 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     //useNewUrlParser: true,
-    //createIndexes: true,
+    //useCreateIndex: true,
     //useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => console.error('DB connection error:', err));
 
-const port = process.env.PORT || 3000;
-
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route to serve login.html when a user navigates to the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','html','login.html'));
+});
+app.get('/public', (req, res) => {
+  res.redirect('/html/registration.html'); // Redirect to a specific page
+});
+
+
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
