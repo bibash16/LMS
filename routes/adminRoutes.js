ecef97e385aa547b71274bc04312edf640b0e5e2
bibash16 <bibhashtamrakar@gmail.com>
2024-04-16 10:34:05 +0545

    feat: protect routes after logging in

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,7 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get('/dashboard', adminController.dashboard);
+router.get('/dashboard', authController.protect, authController.restrictTo('admin'), adminController.dashboard);
 router.post('/logout', authController.postLogout);
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,9 @@ router.get('/registration', authController.getSignUp);
 router.post('/postSignUp', authController.postSignUp);
 router.get('/login', authController.getLogin);
 router.post('/postLogin', authController.postLogin)
-router.get('/dashboard', userController.dashboard);
+router.get('/dashboard',authController.protect, userController.dashboard);
 router.post('/postLeaveApplication', userController.postLeaveApplication);
-router.get('/leave-request', userController.getLeaveApplication);
+router.get('/leave-request',authController.protect, userController.getLeaveApplication);
 router.post('/logout', authController.postLogout);
 
 router.route('/get-user-info')
