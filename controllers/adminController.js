44aa787d6377da0e5ea850675bcfe30219072fbb
Kundan Baidhya <baidyapradip005@gmail.com>
2024-04-18 15:09:53 +0545

    seperates the sidebar to another file

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,14 +6,9 @@ const path = require('path');
 
 exports.dashboard = catchAsync(async (req, res, next) => {
   try {
-    // Fetch user data
-    const users = await User.find({});
     
     // Render the admin dashboard view with user data
-    res.render(path.join(__dirname, '..', 'public', 'html', 'adminHTML', 'adminDashboard.ejs'), {
-      user: req.user,
-      users: users
-    });
+    res.render(path.join(__dirname, '..', 'public', 'html', 'adminHTML', 'adminDashboard.ejs'), {user: req.user});
   } catch (err) {
     // Handle errors
     console.error(err);
@@ -23,11 +18,16 @@ exports.dashboard = catchAsync(async (req, res, next) => {
 
 exports.showProfile = catchAsync(async(req,res,next)=>{
   const users = await User.find({});
-  console.log(users);
-  //console.log(req.user);
   res.render(path.join(__dirname,'..','public','html','adminHTML','adminProfile.ejs'), {user : req.user})
 });
 
+exports.userInfo = catchAsync(async(req,res,next)=>{
+  const users = await User.find({});
+  res.render(path.join(__dirname,'..','public','html','adminHTML','adminUserInfo.ejs'), {
+    user : req.user,
+    users: users
+  })
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
