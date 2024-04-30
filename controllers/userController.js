6669f0ca02ba79b706ca3f295e40bdda362532bf
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-04-30 12:24:30 +0545

    error

--- controllers/userController.js
+++ controllers/userController.js
@@ -105,11 +105,11 @@ exports.leaveRequests = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 3;
 
     // Fetch paginated leave requests in descending order of their creation or update timestamp
-    const { leaves, currentPage, totalPages } = await paginateusermodel(page, limit, -1);
+    const { leave, currentPage, totalPages } = await paginateusermodel(page, limit, -1);
 
     // Render the leave requests view with paginated data
     res.render(path.join(__dirname, '..', 'public', 'html', 'userHTML', 'leaveRequests.ejs'), {
-      leaveRecords: leaves, // Pass the leave records
+      leaveRecords: leave, // Pass the leave records
       currentPage,
       totalPages,
       limit
